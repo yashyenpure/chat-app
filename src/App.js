1c329666c99535d7395acbd454b49efeedb3e91f
yash yenpure <yashyenpureyry1304@gmail.com>
2021-08-12 23:45:12 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
+const App = () => {
+
+
+
+
+  return (
+
+    <ChatEngine
+      height ="100vh"
+      projectID='5e629a47-4638-4c7e-8f95-893196981aea'
+      userName='rajesh123'
+      userSecret='123'
+      renderChatFeed ={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+    )
+}
+    
+export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {useState} from 'react';
+import axios from 'axios';
+
+const LoginForm  = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDeafault();
+
+			const authObject = {'project-ID':"5e629a47-4638-4c7e-8f95-893196981aea" , 'User-Name': username , 'User-Secret': password}
+
+		try {
+			await axios.get('https://api.chatengine.io/chats',{headers: authObject });
+
+			localStorage.setItem('username',username);
+			localStorage.setItem('password',password);
+
+			window.location.reload();
+			setError('')
+
+		} catch(error){
+			setError('oops,incorrect credentials');
+			console.log(error);
+		}
+
+	}
+	return(
+		<div className="wrapper">
+			<div className = "form">
+				<h1 className = "title ">Chat Application</h1>
+				<form onSubmit={handleSubmit}>
+					<input type="text" value ={username} onChange ={(e)=> setUsername(e.target.value)} className = "input" placeholder ="username" required />
+					<input type="text" value ={password} onChange ={(e)=> setPassword(e.target.value)} className = "input" placeholder ="password" required />
+					<div align ="center">
+						<button type ="submit" className = "button">
+							<span>start chatting</span>
+						</button>
+					</div>		
+				</form>
+			</div>	
+			
+		</div>
+
+		)
+
+}
+
+export default LoginForm;
